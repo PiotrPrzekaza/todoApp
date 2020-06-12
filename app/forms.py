aac226e0564cde5fc06bdd5b2a8abff43d1e241b
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-06-12 22:21:36 +0200

    styled all cards

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,8 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget= forms.TextInput(attrs={'placeholder': 'Dodaj nowe zadanie...'}))
+
     class Meta:
         model = Task
         fields = '__all__'
